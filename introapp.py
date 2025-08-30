152e7b728a1936111689b78bcfca805e7d6064b9
Legend-era <98502800+Legend-era@users.noreply.github.com>
2025-08-30 15:04:28 +0530

    Update introapp.py

--- introapp.py
+++ introapp.py
@@ -33,6 +33,11 @@ if not st.session_state.agreed:
 else:
     # --- Main App Code Starts Here ---
     # Load spaCy model
+       try:
+    nlp = spacy.load("en_core_web_sm")
+except OSError:
+    import subprocess
+    subprocess.run(["python", "-m", "spacy", "download", "en_core_web_sm"])
     nlp = spacy.load("en_core_web_sm")
 
     # --- Page Config ---
